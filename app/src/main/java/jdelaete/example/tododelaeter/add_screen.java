868aed07100b7b9a0f8eb3b410d5e847686c4fa3
Votre Nom <Vous@exemple.com>
2019-11-20 12:25:59 +0100

    début du tp

---
+++ app/src/main/java/jdelaete/example/tododelaeter/add_screen.java
@@ -0,0 +1,14 @@
+package jdelaete.example.tododelaeter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class add_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_screen);
+    }
+}
